3f07240df31b9278b71ffc8c9f780076e2f842b9
Utsav Kuntalwad <92204155+Utsavreddy9@users.noreply.github.com>
2026-02-04 01:08:30 -0500

    Add files via upload

---
+++ Utsav Kuntalwad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Utsav Kuntalwad
+//
+//  Created by Utsav Kuntalwad on 1/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Utsav Kuntalwad!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
