e903232d141031e03c66832000b4e1d6a0483eca
Utsav Kuntalwad <92204155+Utsavreddy9@users.noreply.github.com>
2026-02-04 01:07:40 -0500

    Add files via upload

---
+++ Utsav Kuntalwad/Utsav_KuntalwadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Utsav_KuntalwadApp.swift
+//  Utsav Kuntalwad
+//
+//  Created by Utsav Kuntalwad on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct Utsav_KuntalwadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
